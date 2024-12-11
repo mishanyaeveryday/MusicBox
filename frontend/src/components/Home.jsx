2afc1c2cbee8af58e06f88d158e48bc582a0b6a2
Mykhailo Kostenko <mishakostenko70@gmail.com>
2024-12-11 10:30:10 +0100

    Frontend connecting

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -61,6 +61,7 @@ const Playlists = () => {
                 <CardHeader floated={false} className="bg-0">
                 </CardHeader>
                 <CardBody className="text-center">
+                    <Composition />
                 </CardBody>
                 <CardFooter className="flex justify-center gap-7 pt-2"></CardFooter>
             </Card>
@@ -78,6 +79,25 @@ const Compositor = () => {
 const Composition = () => {
     return (
         <div>
+            <Card className="mt-6 w-96">
+                <CardHeader color="" className="">
+                    <img className=''
+                        src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+                        alt="card-image"
+                    />
+                </CardHeader>
+                <CardBody>
+                    <Typography variant="h5" color="" className="mb-2">
+                        Name
+                    </Typography>
+                    <Typography>
+                        Compositor
+                    </Typography>
+                </CardBody>
+                <CardFooter className="pt-0">
+                    <Button>Play</Button>
+                </CardFooter>
+            </Card>
         </div>
     );
 };
