c0c6af7ee708f2483efb7db9cc905c00ef1cd31f
Mykhailo Kostenko <mishakostenko70@gmail.com>
2025-01-07 11:46:17 +0100

    Fixed django-seed

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
+from django.utils import timezone
+import django_seed.guessers
 
 
 class CoreConfig(AppConfig):
@@ -7,3 +9,10 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         import core.signals
+
+
+def _timezone_format(value):
+    return timezone.make_aware(value, timezone.get_current_timezone())
+
+
+django_seed.guessers._timezone_format = _timezone_format
